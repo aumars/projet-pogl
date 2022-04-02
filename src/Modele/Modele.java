5aa4af84fab4e49aec8e55ff593dd7a61b58022e
Pradana AUMARS <paumars@courrier.dev>
2022-04-02 16:08:30 +0200

    Permettre le Modele d'avoir un ensemble de Personnages.
    * src/Modele/Modele.java: Ajouter un attribut ensemble.

--- src/Modele/Modele.java
+++ src/Modele/Modele.java
@@ -1,10 +1,12 @@
 package Modele;
 
 import java.util.Observable;
+import java.util.Set;
 
 public class Modele extends Observable {
     private static final int largeur = 8, hauteur = 8;
     private Grille grille;
+    private Set<Personnage> ensemble;
     private Joueur joueur;
     public Modele() {
         this.grille = new Grille(hauteur, largeur);
