9e2a0bf67c2fdf28736c0799997e3f2fa5cad943
Pradana AUMARS <paumars@courrier.dev>
2022-04-03 01:44:30 +0200

    Ajouter les Artefacts et les Clefs dans la grille.
    * src/Modele/Grille.java (Grille): Ajouté des objets.

--- src/Modele/Grille.java
+++ src/Modele/Grille.java
@@ -13,6 +13,16 @@ public class Grille {
                 this.cases[i][j] = new Case(i, j, i >= 1 && i <= hauteur - 1 && j >= 1 && j <= largeur - 1, i == 4 && j == 5);
             }
         }
+
+        this.cases[2][3].ajoutObjet(new Artefact(Element.AIR));
+        this.cases[3][4].ajoutObjet(new Artefact(Element.EAU));
+        this.cases[6][4].ajoutObjet(new Artefact(Element.TERRE));
+        this.cases[5][1].ajoutObjet(new Artefact(Element.FEU));
+
+        this.cases[4][2].ajoutObjet(new Clef(Element.AIR));
+        this.cases[5][6].ajoutObjet(new Clef(Element.EAU));
+        this.cases[5][3].ajoutObjet(new Clef(Element.TERRE));
+        this.cases[1][6].ajoutObjet(new Clef(Element.FEU));
     }
 
     public boolean estTraversable(int x, int y) {
