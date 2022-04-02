6b5f6f73b51ea665261d781a009a59c99db0efb3
Pradana AUMARS <paumars@courrier.dev>
2022-04-02 18:02:13 +0200

    Mettre à jour Case pour avoir Terrain.
    * src/Modele/Case.java (mer): Remplacé avec terrain.
    (Case): Initialise terrain.

--- src/Modele/Case.java
+++ src/Modele/Case.java
@@ -6,11 +6,16 @@ public class Case {
     private int positionX;
     private int positionY;
     private Inondation etat = Inondation.SECHE;
-    private boolean mer;
+    private Terrain terrain;
 
     public Case(int i, int j, boolean m) {
         this.positionX = i;
         this.positionY = j;
-        this.mer = m;
+        if (m) {
+            this.terrain = Terrain.TERRE;
+        }
+        else {
+            this.terrain = Terrain.MER;
+        }
     }
 }
