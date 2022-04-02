302e70a13cd725b479429dad03cb75cfcba30bb4
Pradana AUMARS <paumars@courrier.dev>
2022-04-02 18:23:45 +0200

    Implmenter initialise, deplaceJoueur.
    * src/Modele/Modele.java (Modele): Déplacé quelques lignes en initialise.
    (initialise): Nouvelle méthode.
    (deplaceJoueur): Nouvelle méthode.

--- src/Modele/Modele.java
+++ src/Modele/Modele.java
@@ -1,5 +1,6 @@
 package Modele;
 
+import java.util.HashSet;
 import java.util.Observable;
 import java.util.Set;
 
@@ -8,7 +9,33 @@ public class Modele extends Observable {
     private Grille grille;
     private Set<Personnage> ensemble;
     private Joueur joueur;
+
     public Modele() {
         this.grille = new Grille(hauteur, largeur);
+        this.initialise();
+    }
+
+    public void initialise() {
+        this.joueur = new Joueur(hauteur-2, largeur-2);
+        this.ensemble = new HashSet<>();
+        this.ensemble.add(this.joueur);
+    }
+
+    public boolean deplaceJoueur(Direction dir) {
+        int x = this.joueur.posX();
+        int y = this.joueur.posY();
+        switch (dir) {
+            case HAUT: x--; break;
+            case BAS: x++; break;
+            case GAUCHE: y--; break;
+            case DROITE: y++; break;
+        }
+        if (this.grille.estTraversable(x, y)) {
+            this.joueur.deplace(dir);
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
