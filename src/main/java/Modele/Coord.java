d31016c07f37c0860177d20819ff3dccdd1ca24f
Pradana AUMARS <paumars@courrier.dev>
2022-04-04 15:01:13 +0200

    Implémenter la classe Coord.
    * src/main/java/Modele/Coord.java: Nouvelle classe.

---
+++ src/main/java/Modele/Coord.java
@@ -0,0 +1,29 @@
+package Modele;
+
+public class Coord {
+    private int x, y;
+    public Coord(int x, int y) {
+        if (x < 0 || y < 0) {
+            throw new IllegalArgumentException("");
+        }
+        this.x = x;
+        this.y = y;
+    }
+    public int x() { return this.x; }
+    public int y() { return this.y; }
+    public void translate(Direction dir) {
+        switch (dir) {
+            case HAUT: this.y--;
+            case BAS: this.y++;
+            case GAUCHE: this.x--;
+            case DROITE: this.x++;
+        }
+    }
+    public String toString() { return String.format("(%d, %d)", this.x, this.y); }
+    public boolean equals(Object obj) {
+        if (this == obj) { return true; }
+        if (obj == null || (obj.getClass() != this.getClass())) { return false; }
+        Coord c = (Coord) obj;
+        return this.x == c.x() && this.y == c.y();
+    }
+}
