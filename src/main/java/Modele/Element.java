f30319affe11b097cdf28c700b32df88f4f536d9
Pradana AUMARS <paumars@courrier.dev>
2022-04-04 15:22:19 +0200

    Implémenter méthode pour initialiser Element à partir d'un String.
    * src/main/java/Modele/Element.java (StringToElement): Nouvelle méthode statique.

--- src/main/java/Modele/Element.java
+++ src/main/java/Modele/Element.java
@@ -4,5 +4,13 @@ enum Element {
     AIR,
     EAU,
     TERRE,
-    FEU
+    FEU;
+
+    public static Element StringToElement(String s) {
+        if (s.equals("AIR")) return Element.AIR;
+        else if (s.equals("EAU")) return Element.EAU;
+        else if (s.equals("TERRE")) return Element.TERRE;
+        else if (s.equals("FEU")) return Element.FEU;
+        else return null;
+    }
 }
