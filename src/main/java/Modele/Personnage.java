beba9d836f77d71e992ffb412accd3868cbdfc33
Pradana AUMARS <paumars@courrier.dev>
2022-04-04 15:16:49 +0200

    Implémenter Coord dans Personnage.
    * src/main/java/Modele/Personnage.java (posX): Supprimé.
    (posY): Supprimé.
    (deplace). Supprimé.
    
    On peut accèder Coord directement à la place.

--- src/main/java/Modele/Personnage.java
+++ src/main/java/Modele/Personnage.java
@@ -2,25 +2,13 @@ package Modele;
 
 class Personnage {
     private boolean estVivant = true;
-    private int positionX;
-    private int positionY;
+    public final Coord coord;
 
     public Personnage(int x, int y) {
-        this.positionX = x;
-        this.positionY = y;
+        this.coord = new Coord(x, y);
     }
 
     public void meurt() {
         this.estVivant = false;
-    }
-    public int posX() { return this.positionX; }
-    public int posY() { return this.positionY; }
-    void deplace(Direction dir) {
-        switch (dir) {
-            case HAUT: this.positionX--;
-            case BAS: this.positionX++;
-            case GAUCHE: this.positionY--;
-            case DROITE: this.positionY++;
-        }
     }
 }
