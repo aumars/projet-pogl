def69c8895cef912a2fbea05cb2c19cc572966bd
Pradana AUMARS <paumars@courrier.dev>
2022-04-04 15:04:28 +0200

    Implémenter méthode statique pour initialiser des objets en fonction d'un String.
    * src/main/java/Modele/Objet.java (objetById): Nouvelle méthode.

--- src/main/java/Modele/Objet.java
+++ src/main/java/Modele/Objet.java
@@ -5,4 +5,17 @@ abstract class Objet {
     public Objet(Element el) {
         this.element = el;
     }
+    public static Objet objetByID(String id, Element e) {
+        Objet o;
+        if (id.equals("artefact")) {
+            o = new Artefact(e);
+        }
+        else if (id.equals("clef")) {
+            o = new Clef(e);
+        }
+        else {
+            throw new IllegalArgumentException(String.format("%s n'est pas un objet reconnu.", id));
+        }
+        return o;
+    }
 }
