bf33e703baad44835a5e2261954be0752a741b5a
Manitas Bahri <manitas.bhr@gmail.com>
2022-04-11 10:04:45 +0200

    Ajout observable et observer

---
+++ src/main/java/Modele/Observable.java
@@ -0,0 +1,22 @@
+package Modele;
+
+import java.util.ArrayList;
+import Vue.Observer;
+
+abstract class Observable {
+    private ArrayList<Observer> observers;
+
+    public Observable (){
+        this.observers = new ArrayList<Observer>();
+    }
+
+    public void addObserver(Observer o){
+        this.observers.add(o);
+    }
+
+    public void notifyObservers(){
+        for (Observer observer : this.observers) {
+            observer.update();
+        }
+    }
+}
