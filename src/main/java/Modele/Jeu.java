a25c4e8e0e6f509ea3c54cfe8bf654dc9b6dd278
Pradana AUMARS <paumars@courrier.dev>
2022-04-14 17:08:13 +0200

    Ajouter la documentation à Jeu.

--- src/main/java/Modele/Jeu.java
+++ src/main/java/Modele/Jeu.java
@@ -13,18 +13,44 @@ import java.io.IOException;
 import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
+/**
+ * Une classe qui modélise les coordonnées de tous les {@link Objet}s et {@link Joueur}s dans un jeu.
+ */
 public final class Jeu {
+    /**
+     * Une liste qui associe des {@link Objet}s à son {@link Coord}.
+     */
     public final List<AbstractMap.SimpleImmutableEntry<Objet, Coord>> objets;
+
+    /**
+     * Une liste qui associe des {@link Joueur}s à son {@link Coord}.
+     */
     public final List<AbstractMap.SimpleImmutableEntry<Joueur, Coord>> ensemble;
 
-    public Jeu(String game_path) throws ParserConfigurationException, IOException, SAXException, InvalidGameException {
+    /**
+     * Modélise un jeu (avec {@link Joueur}s) à partir d'un fichier XML.
+     * @param game_path Chemin du fichier XML.
+     * @throws InvalidGameException S'il existe une erreur avec le fichier XML.
+     */
+    public Jeu(String game_path) throws InvalidGameException {
         this(game_path, false);
     }
 
-    public Jeu(String game_path, boolean vide) throws ParserConfigurationException, IOException, SAXException, InvalidGameException {
-        Document game = parseGame(game_path);
+    /**
+     * Modélise un jeu (avec {@link Joueur}s) à partir d'un fichier XML.
+     * @param game_path Chemin du fichier XML.
+     * @param vide S'il y a des {@link Joueur}s dans le jeu.
+     * @throws InvalidGameException S'il existe une erreur avec le fichier XML.
+     */
+    public Jeu(String game_path, boolean vide) throws InvalidGameException {
+        Document game;
+        try {
+            game = parseGame(game_path);
+        }
+        catch (ParserConfigurationException | IOException | SAXException e) {
+            throw new InvalidGameException(e.getMessage(), e.getCause());
+        }
         this.objets = parseGameObjet(game);
         if (vide) {
             this.ensemble = new ArrayList<>();
@@ -34,12 +60,23 @@ public final class Jeu {
         }
     }
 
+    /**
+     * Lit un fichier XML.
+     * @param game_path Chemin du fichier XML.
+     * @return Le document XML.
+     */
     private static Document parseGame(String game_path) throws ParserConfigurationException, IOException, SAXException {
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         DocumentBuilder db = dbf.newDocumentBuilder();
-        return db.parse(Objects.requireNonNull(Modele.class.getClassLoader().getResourceAsStream(game_path)));
+        return db.parse(Modele.class.getClassLoader().getResourceAsStream(game_path));
     }
 
+    /**
+     * Lit le document XML pour les {@link Objet}s et leur position sur un plan.
+     * @param game Document XML.
+     * @return Liste de pairs d'{@link Objet} et d'{@link Coord} associée.
+     * @throws InvalidGameException Deux {@link Objet}s sont sur le même {@link Coord}.
+     */
     private static List<AbstractMap.SimpleImmutableEntry<Objet, Coord>> parseGameObjet(Document game) throws InvalidGameException {
         List<AbstractMap.SimpleImmutableEntry<Objet, Coord>> a = new ArrayList<>();
         NodeList list = game.getElementsByTagName("objet");
@@ -52,7 +89,7 @@ public final class Jeu {
                 Coord pos = parsePosition(element.getElementsByTagName("position"));
                 for (AbstractMap.SimpleImmutableEntry<Objet, Coord> ex: a) {
                     if (ex.getValue().equals(pos)) {
-                        throw new InvalidGameException();
+                        throw new InvalidGameException(String.format("Deux objets sont sur le même point %s", pos));
                     }
                 }
                 Element e = elementById(objet_element);
@@ -61,6 +98,45 @@ public final class Jeu {
             }
         }
         return a;
+    }
+
+    /**
+     * Lit le document XML pour les {@link Joueur}s et leur position sur un plan.
+     * @param game Document XML.
+     * @return Liste de pairs d'{@link Joueur} et d'{@link Coord} associée.
+     */
+    private static List<AbstractMap.SimpleImmutableEntry<Joueur, Coord>> parseGameJoueur(Document game) {
+        List<AbstractMap.SimpleImmutableEntry<Joueur, Coord>> s = new ArrayList<>();
+        NodeList list = game.getElementsByTagName("personnage");
+        for (int i = 0; i < list.getLength(); i++) {
+            Node node = list.item(i);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                org.w3c.dom.Element element = (org.w3c.dom.Element) node;
+                String id = element.getAttribute("id");
+                Coord pos = parsePosition(element.getElementsByTagName("position"));
+                Joueur j = joueurById(id);
+                s.add(new AbstractMap.SimpleImmutableEntry<>(j, pos));
+            }
+        }
+        return s;
+    }
+
+    /**
+     * Construit un {@link Coord}.
+     * @param pos Position du noeud XML "position".
+     * @return Un {@link Coord}
+     */
+    private static Coord parsePosition(NodeList pos) {
+        for (int i = 0; i < pos.getLength(); i++) {
+            Node node = pos.item(i);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                org.w3c.dom.Element element = (org.w3c.dom.Element) node;
+                int x = Integer.parseInt(element.getElementsByTagName("x").item(0).getTextContent());
+                int y = Integer.parseInt(element.getElementsByTagName("y").item(0).getTextContent());
+                return new Coord(x, y);
+            }
+        }
+        throw new RuntimeException();
     }
 
     /**
@@ -85,10 +161,10 @@ public final class Jeu {
 
     /**
      * Construit un objet associé à un élément selon son id.
-     * @param id
-     * @param e
-     * @return
-     * @exception IllegalArgumentException If `s` is not recognised.
+     * @param id Une chaine de caractères
+     * @param e Un {@link Element}
+     * @return Un {@link Objet}
+     * @exception IllegalArgumentException Si {@code s} n'est pas reconnu.
      */
     private static Objet objetsByID(String id, Element e) {
         switch (id) {
@@ -101,40 +177,16 @@ public final class Jeu {
         }
     }
 
-    private static List<AbstractMap.SimpleImmutableEntry<Joueur, Coord>> parseGameJoueur(Document game) {
-        List<AbstractMap.SimpleImmutableEntry<Joueur, Coord>> s = new ArrayList<>();
-        NodeList list = game.getElementsByTagName("personnage");
-        for (int i = 0; i < list.getLength(); i++) {
-            Node node = list.item(i);
-            if (node.getNodeType() == Node.ELEMENT_NODE) {
-                org.w3c.dom.Element element = (org.w3c.dom.Element) node;
-                String id = element.getAttribute("id");
-                Coord pos = parsePosition(element.getElementsByTagName("position"));
-                Joueur j = joueurById(id);
-                s.add(new AbstractMap.SimpleImmutableEntry<>(j, pos));
-            }
-        }
-        return s;
-    }
-
+    /**
+     * Construit un {@link Joueur} selon son id.
+     * @param name Une chaine de caractères
+     * @return Un {@link Joueur}
+     */
     private static Joueur joueurById(String name) {
         if (name.equals("joueur")) {
             return new Joueur();
         } else {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(String.format("%s n'est pas un joueur reconnu", name));
         }
-    }
-
-    private static Coord parsePosition(NodeList pos) {
-        for (int i = 0; i < pos.getLength(); i++) {
-            Node node = pos.item(i);
-            if (node.getNodeType() == Node.ELEMENT_NODE) {
-                org.w3c.dom.Element element = (org.w3c.dom.Element) node;
-                int x = Integer.parseInt(element.getElementsByTagName("x").item(0).getTextContent());
-                int y = Integer.parseInt(element.getElementsByTagName("y").item(0).getTextContent());
-                return new Coord(x, y);
-            }
-        }
-        throw new RuntimeException();
     }
 }
