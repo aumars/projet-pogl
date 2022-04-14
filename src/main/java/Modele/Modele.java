db30e3700991c025295bb91acab8e96463961ff6
Pradana AUMARS <paumars@courrier.dev>
2022-04-14 17:22:08 +0200

    Ajouter la documentation à Modèle.

--- src/main/java/Modele/Modele.java
+++ src/main/java/Modele/Modele.java
@@ -1,19 +1,42 @@
 package Modele;
 
 import Modele.Exception.InvalidGameException;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
 import java.util.*;
 
+/**
+ * Le système de jeu.
+ */
 public class Modele extends Observable {
+    /**
+     * La grille du jeu.
+     */
     private final Grille grille;
+
+    /**
+     * L'ensemble de joueurs.
+     */
     private final List<Joueur> ensemble;
+
+    /**
+     * Un itérateur de l'ensemble de joueurs.
+     */
     private Iterator<Joueur> iter;
+
+    /**
+     * Le nombre de tours parcourus.
+     */
     private int tour;
+
+    /**
+     * Le joueur actuel.
+     */
     private Joueur joueurActuel;
 
+    /**
+     * Construit un jeu à partir d'une carte et l'ensemble d'objets et de joueurs.
+     * @param carte Une carte
+     * @param jeu L'ensemble d'objets et de joueurs.
+     */
     public Modele(Carte carte, Jeu jeu) {
         this.grille = new Grille(carte);
         this.grille.addObjets(jeu.objets);
@@ -29,32 +52,56 @@ public class Modele extends Observable {
         this.iter = this.ensemble.iterator();
     }
 
-    public Modele (String map_path, String game_path) throws ParserConfigurationException, IOException, SAXException, InvalidGameException {
+    /**
+     * Construit un jeu à partir d'un fichier texte et d'un fichier XML.
+     * @param map_path Un fichier texte
+     * @param game_path Un fichier XML
+     */
+    public Modele (String map_path, String game_path) throws InvalidGameException {
         this(new Carte(map_path), new Jeu(game_path));
     }
 
+    /**
+     * Renvoie le joueur actuel.
+     * @return Le joueur actuel.
+     */
     public Joueur getJoueurActuel() { return this.joueurActuel; }
 
+    /**
+     * Renvoie le nombre de joueurs au total.
+     * @return Renvoie le nombre de joueurs au total.
+     */
     public int getNbJoueurs(){
         return this.ensemble.size();
     }
 
-    public boolean tourSuivant() {
+    /**
+     * Incrémente au prochain tour.
+     */
+    public void tourSuivant() {
         this.tour++;
         this.grille.inonde();
         for (Joueur joueur: this.ensemble) { joueur.noie(); }
-        if (this.ensemble.stream().noneMatch(joueur -> joueur.estVivant())) {
-            return false;
-        }
         this.joueurActuel = this.prochainJoueurVivant();
         this.joueurActuel.newTurn();
-        return true;
     }
 
+    /**
+     * Renvoie le tour actuel.
+     * @return Le tour actuel.
+     */
     public int getTour() { return this.tour; }
 
+    /**
+     * Renvoie la grille.
+     * @return La grille.
+     */
     public Grille getGrille(){ return this.grille; }
 
+    /**
+     * Vérifie qui est le joueur gagnant.
+     * @return le joueur gagnant, null s'il n'y a pas.
+     */
     public Joueur verifieGagnants() {
         for (Joueur j: this.ensemble) {
             if (j.verifieGagnant()) {
@@ -64,6 +111,10 @@ public class Modele extends Observable {
         return null;
     }
 
+    /**
+     * Calcule le joueur prochain vivant.
+     * @return le joueur prochain vivant.
+     */
     private Joueur prochainJoueurVivant() {
         if (!this.iter.hasNext()) {
             this.iter = this.ensemble.iterator();
@@ -78,13 +129,11 @@ public class Modele extends Observable {
         return joueur;
     }
 
+    /**
+     * Vérifie si tous les joueurs sont morts.
+     * @return Vrai si tous les joueurs sont morts, Faux sinon.
+     */
     public boolean tousJoueursMorts(){
-        for (Joueur j: this.ensemble) {
-            if (j.estVivant()) {
-                return false;
-            }
-        }
-
-        return true;
+        return this.ensemble.stream().noneMatch(j -> j.estVivant());
     }
 }
