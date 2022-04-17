25ca781a5adb5c4754804ad5a6a7e89e3732f7fb
Pradana AUMARS <paumars@courrier.dev>
2022-04-17 12:01:40 +0200

    Ajouter game1.xml (4 joueurs) et game2.xml (1 joueur).

--- src/test/java/Modele/ModeleTest.java
+++ src/test/java/Modele/ModeleTest.java
@@ -6,10 +6,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ModeleTest {
-    Modele modeleM1G1;
+    Modele modeleM1G1, modeleM1G2;
     @BeforeEach
     void modeleInit() throws InvalidGameException {
         this.modeleM1G1 = new Modele("map1.txt", "game1.xml");
+        this.modeleM1G2 = new Modele("map1.txt", "game2.xml");
     }
 
     @Test
@@ -29,14 +30,83 @@ public class ModeleTest {
     }
 
     @Test
+    void modeleGetNbJoueursGame1() {
+        Assertions.assertEquals(4, this.modeleM1G1.getNbJoueurs());
+    }
+
+    @Test
+    void modeleGetNbJoueursGame2() {
+        Assertions.assertEquals(1, this.modeleM1G2.getNbJoueurs());
+    }
+
+    @Test
+    void modeleGetJoueurActuelGame1() {
+        Modele m = this.modeleM1G1;
+        Joueur j1 = m.getJoueurActuel();
+        m.tourSuivant();
+        Joueur j2 = m.getJoueurActuel();
+        m.tourSuivant();
+        Joueur j3 = m.getJoueurActuel();
+        m.tourSuivant();
+        Joueur j4 = m.getJoueurActuel();
+        m.tourSuivant();
+        Joueur j5 = m.getJoueurActuel();
+        Assertions.assertNotSame(j1, j2);
+        Assertions.assertNotSame(j1, j3);
+        Assertions.assertNotSame(j1, j4);
+        Assertions.assertSame(j1, j5);
+    }
+
+    @Test
+    void modeleTestTourSuivantGame1() {
+        Modele m = this.modeleM1G1;
+        m.getJoueurActuel().deplace(Direction.DROITE);
+        Assertions.assertEquals(new Coord(2, 1), m.getJoueurActuel().getCoord());
+        m.getJoueurActuel().deplace(Direction.DROITE);
+        Assertions.assertEquals(new Coord(2, 1), m.getJoueurActuel().getCoord());
+        m.tourSuivant();
+        m.getJoueurActuel().deplace(Direction.DROITE);
+        Assertions.assertEquals(new Coord(2, 1), m.getJoueurActuel().getCoord());
+    }
+
+    @Test
+    void modeleTestTourSuivantGame2() {
+        Modele m = this.modeleM1G2;
+        m.getJoueurActuel().deplace(Direction.DROITE);
+        Assertions.assertEquals(new Coord(2, 1), m.getJoueurActuel().getCoord());
+        m.getJoueurActuel().deplace(Direction.DROITE);
+        Assertions.assertEquals(new Coord(2, 1), m.getJoueurActuel().getCoord());
+        m.tourSuivant();
+        m.getJoueurActuel().deplace(Direction.DROITE);
+        Assertions.assertEquals(new Coord(3, 1), m.getJoueurActuel().getCoord());
+    }
+
+    @Test
     void modeleTestGame1() {
-        this.modeleM1G1.getJoueurActuel().deplace(Direction.DROITE);
-        this.modeleM1G1.tourSuivant();
-        this.modeleM1G1.getJoueurActuel().deplace(Direction.DROITE);
-        this.modeleM1G1.tourSuivant();
-        this.modeleM1G1.getJoueurActuel().deplace(Direction.DROITE);
-        this.modeleM1G1.tourSuivant();
-        this.modeleM1G1.getJoueurActuel().deplace(Direction.BAS);
-        Assertions.assertNotNull(this.modeleM1G1.getJoueurActuel().chercheCle());
+        Modele m = this.modeleM1G1;
+        m.getJoueurActuel().deplace(Direction.DROITE);
+        Assertions.assertEquals(new Coord(2, 1), m.getJoueurActuel().getCoord());
+        m.tourSuivant();
+        m.getJoueurActuel().deplace(Direction.DROITE);
+        Assertions.assertEquals(new Coord(2, 1), m.getJoueurActuel().getCoord());
+        m.tourSuivant();
+        m.getJoueurActuel().deplace(Direction.DROITE);
+        Assertions.assertEquals(new Coord(2, 1), m.getJoueurActuel().getCoord());
+        m.tourSuivant();
+        m.getJoueurActuel().deplace(Direction.DROITE);
+        Assertions.assertEquals(new Coord(2, 1), m.getJoueurActuel().getCoord());
+    }
+
+    @Test
+    void modeleTestGame2() {
+        Modele m = this.modeleM1G2;
+        m.getJoueurActuel().deplace(Direction.DROITE);
+        m.tourSuivant();
+        m.getJoueurActuel().deplace(Direction.DROITE);
+        m.tourSuivant();
+        m.getJoueurActuel().deplace(Direction.DROITE);
+        m.tourSuivant();
+        m.getJoueurActuel().deplace(Direction.BAS);
+        Assertions.assertNotNull(m.getJoueurActuel().chercheCle());
     }
 }
