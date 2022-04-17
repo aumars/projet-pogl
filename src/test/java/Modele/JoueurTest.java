b20dfe715c5d0766bb7ee8449fd11752d2334083
Pradana AUMARS <paumars@courrier.dev>
2022-04-17 04:07:10 +0200

    Ajouter des tests de Joueur.

---
+++ src/test/java/Modele/JoueurTest.java
@@ -0,0 +1,41 @@
+package Modele;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class JoueurTest {
+    Grille g;
+    Joueur j0, j1;
+
+    @BeforeEach
+    void joueurInit() {
+        this.g = new Grille(new Carte("map1.txt"));
+        this.j0 = new Joueur();
+        this.j1 = new Joueur(this.g.getCase(new Coord(1, 1)));
+    }
+
+    @Test
+    void joueurSurCaseTraversable() {
+        Assertions.assertThrows(RuntimeException.class, () -> this.j0.surCaseTraversable());
+        Assertions.assertTrue(this.j1.surCaseTraversable());
+    }
+
+    @Test
+    void joueurPossedeClef() {
+        Assertions.assertFalse(this.j0.possedeClef(Element.AIR));
+        Assertions.assertFalse(this.j1.possedeClef(Element.AIR));
+    }
+
+    @Test
+    void joueurDeplace() {
+        Assertions.assertThrows(RuntimeException.class, () -> this.j0.verifieGagnant());
+        Assertions.assertFalse(this.j1.verifieGagnant());
+    }
+
+    @Test
+    void joueurPrendObjet() {
+        Assertions.assertThrows(RuntimeException.class, () -> this.j0.prendObjet());
+        Assertions.assertNull(this.j1.prendObjet());
+    }
+}
