3873ba68aeb6c533273819a6c10e2111f53a8de4
Pradana AUMARS <paumars@courrier.dev>
2022-04-20 11:06:44 +0200

    Ajouter noms des Joueurs

--- src/main/java/Vue/ConstsValue.java
+++ src/main/java/Vue/ConstsValue.java
@@ -14,20 +14,9 @@ public final class ConstsValue {
     // Les fonts
     public static String FONT_FAMILY = "Arial";
 
-    // Les courleurs.
+    // Les couleurs.
     public static Color COLOR_MER = new Color(47, 128, 124);
     public static Color COLOR_SEC = new Color(48, 97, 14);
     public static Color COLOR_INONDE = new Color(101, 147, 99);
-    public static Color COLOR_DEFAULT = new Color(238, 238, 238);
-
-    /**
-     * Récupère le nom associé au joueurs avec l'id i.
-     * 
-     * @param i L'ID du joueurs.
-     * @return
-     */
-    public static String getNomJoueur(int i) {
-        String[] avatars = { "Jack", "Hector", "Ninja", "Pinky" };
-        return avatars[i];
-    }
+    public static Color COLOR_DEFAULT = new Color(246, 246, 246);
 }
